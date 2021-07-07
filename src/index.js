30f3e4aedc53e72fdf09b652a6227b0ad9b263d0
Feliks1991 <1844364@gmail.com>
2021-07-08 00:06:02 +0300

    setting3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-export default function sum(a, b) {
-    return a + b;
-}
+function promiseTest() {
+    const getWeather = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const weatherData = { temperature: "30*" };
+        const condition = true;
+        if (condition) resolve(weatherData);
+        else reject(new Error("no weather data"));
+      }, 1000);
+    });
+  
+    getWeather.then((data) => console.log(data));
+  }
+  promiseTest(); 
